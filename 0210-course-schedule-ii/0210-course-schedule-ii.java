ccc3fb66d525f72607fca2109acc8a08b934aa79
Vaibhav Varshney <113897766+vaibhav0415@users.noreply.github.com>
2025-03-22 18:18:17 +0530

    Time: 5 ms (79.12%), Space: 46.1 MB (38.72%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findOrder(int n, int[][] pre) {
+         List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int[] indegree=new int[n];
+        
+        for(int i=0;i<pre.length;i++){
+            int a=pre[i][0],b=pre[i][1] ;//edges from b->a
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        Queue<Integer> q=new ArrayDeque<>();
+         List<Integer> ans=new ArrayList<>();
+         for(int i=0;i<n;i++){
+            if(indegree[i]==0) {
+                q.add(i);
+               
+                }
+         }
+         while(q.size()>0){
+            int front=q.remove();
+             ans.add(front);
+             for(int ele:adj.get(front)){
+                    indegree[ele]--;
+                    if(indegree[ele]==0) q.add(ele);
+                    
+             }
+         }
+         int ans2[]=new int[ans.size()];
+         for(int i=0;i<ans.size();i++){
+            ans2[i]=ans.get(i);
+         }
+         if(ans.size()!=n) return new int[0];
+         else return  ans2;
+
+
+    }
+}
