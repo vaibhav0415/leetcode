6464a44b14a6e5e3743b31869a5ddc8f9fd7fce1
Vaibhav Varshney <113897766+vaibhav0415@users.noreply.github.com>
2025-03-22 23:36:35 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.4%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1 || n==2) return 1;
+      else{
+        int a=0;
+        int b=1;
+        int c=1;
+        int d=-1;
+        for(int i=3;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+            
+            
+        }
+        return d;
+      }
+    }
+}
