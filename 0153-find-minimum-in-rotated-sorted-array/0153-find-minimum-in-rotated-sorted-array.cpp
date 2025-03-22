8ca298d680f56167cb43b11ea5fa61480ef3b910
Vaibhav Varshney <113897766+vaibhav0415@users.noreply.github.com>
2025-03-22 18:01:24 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.16%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo=0;
+        int hi=nums.size()-1;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]>nums[hi]) lo=mid+1;
+            else {//nums[mid]<nums[lo] matlab hamara jo min element hai wo  
+                 hi=mid;
+            }
+        }
+        return nums[lo];
+    }
+};
